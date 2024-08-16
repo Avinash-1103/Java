6f1088e4bbe8d460d316ef7a4fcba7a9c6d56e3d
Avinash_Nalawade <151865054+Avinash-1103@users.noreply.github.com>
2024-08-17 02:16:35 +0530

    Delete program6.java

--- program6.java
+++
@@ -1,9 +0,0 @@
-// Print numbers between 101 to 110
-
-public class program6 {
-    public static void main(String[] args) {
-        for (int i = 101; i < 111; i++) {
-            System.out.println(i);
-        }
-    }
-}
