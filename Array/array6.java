67798629181b92dd65671aadf57df8e8308c0923
Avinash_Nalawade <151865054+Avinash-1103@users.noreply.github.com>
2024-09-30 23:24:22 +0530

    Add files via upload

---
+++ Array/array6.java
@@ -0,0 +1,33 @@
+public class array6 {
+    public static void main(String[] args) {
+
+        // initializing array
+        int[] Num = new int[5];
+
+        // Displaying Null Array
+
+        System.out.println("\n Value of Element 1 = " + Num[0] + ".");
+        System.out.println("\n Value of Element 2 = " + Num[1] + ".");
+        System.out.println("\n Value of Element 3 = " + Num[2] + ".");
+        System.out.println("\n Value of Element 4 = " + Num[3] + ".");
+        System.out.println("\n Value of Element 5 = " + Num[4] + ".");
+
+        /// Initializing Array Elements
+
+        Num[0] = 10;
+        Num[1] = 20;
+        Num[3] = 77;
+        Num[4] = 21;
+
+        System.out.println("\n Changed Values in Array are => \n");
+
+        /// Displaying Array With New Values
+
+        System.out.println("\n Value of Element 1 = " + Num[0] + ".");
+        System.out.println("\n Value of Element 2 = " + Num[1] + ".");
+        System.out.println("\n Value of Element 3 = " + Num[2] + ".");
+        System.out.println("\n Value of Element 4 = " + Num [3] + ".");
+        System.out.println("\n Value of Element 5 = " + Num[4] + ".");
+
+    }
+}
